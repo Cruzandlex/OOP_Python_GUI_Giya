e4ec36063fc669c4afbe80cf8fadedf469b42594
Cruzandlex <Cruzandlex@gmail.com>
2020-03-09 23:28:23 +0800

    UserSide Done

--- TourGuideUi.py
+++ TourGuideUi.py
@@ -19,36 +19,40 @@ class App(QMainWindow):
             background-color:#1C1F21;
         }
         QLabel#Texts{
-        color:#CCF20D;
+        color:#E9ED68;background-color:#000000;
         padding-left:5px;
-        background-color:#000000;
-        font-size: 18px;
-        border-color: red;
-        
+        font-size: 20px;
         }
         """
         self.setStyleSheet(self.stylesheet)
         self.show()
     def TourGuideUI(self):
+        self.UserSide()
+        self.RatingSide()
+        self.show()
+    def UserSide(self):
         self.setWindowTitle("TOUR GUIDE UI")
         self.setGeometry(self.x,self.y,self.width,self.height)
         self.Icon=QLabel(self)
         pixmap=QPixmap('BlankIcon.ico')
         self.Icon.setPixmap(pixmap)
-        self.Icon.setGeometry(150,100,200,150)
+        self.Icon.setGeometry(150,100,400,150)
+        self.Icon.setStyleSheet("""
+        border-radius: 25px;
+        border: 2px solid #E9ED68;background: #FFFFFF;
+        padding: 20px;width: 200px;height: 150px;
+        """)
         self.nameBox=QLabel("Driver Name",self)
-        self.nameBox.setGeometry(300,112,200,28)
+        self.nameBox.setGeometry(310,122,220,28)
         self.HomeLocation=QLabel("Location/Region",self)
-        self.HomeLocation.setGeometry(300,147,200,28)
+        self.HomeLocation.setGeometry(310,161,220,28)
         self.Loyalty=QLabel("Loyalty level:   ",self)
-        self.Loyalty.setGeometry(300,182,200,28)
+        self.Loyalty.setGeometry(310,200,220,28)
         self.nameBox.setObjectName("Texts")
         self.HomeLocation.setObjectName("Texts")
         self.Loyalty.setObjectName("Texts")
+    def RatingSide(self):
 
-        self.show()
-
-        
     
 
     #@pyqtSlot()
