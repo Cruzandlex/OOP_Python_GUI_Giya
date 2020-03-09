8c1dd0702212713ad0e29c01b9f72be401cdbef8
Cruzandlex <Cruzandlex@gmail.com>
2020-03-09 22:46:17 +0800

    Text Update

--- TourGuideUi.py
+++ TourGuideUi.py
@@ -16,12 +16,18 @@ class App(QMainWindow):
         self.TourGuideUI()
         self.stylesheet= """
         QMainWindow{
-            background-color:color:#1C1F21;
+            background-color:#1C1F21;
         }
-        QLabel{
-            background-color: ;color:#CCF20D;padding-left:5px;
+        QLabel#Texts{
+        color:#CCF20D;
+        padding-left:5px;
+        background-color:#000000;
+        font-size: 18px;
+        border-color: red;
+        
         }
         """
+        self.setStyleSheet(self.stylesheet)
         self.show()
     def TourGuideUI(self):
         self.setWindowTitle("TOUR GUIDE UI")
@@ -29,11 +35,17 @@ class App(QMainWindow):
         self.Icon=QLabel(self)
         pixmap=QPixmap('BlankIcon.ico')
         self.Icon.setPixmap(pixmap)
-        self.Icon.setGeometry(150,0,200,150)
+        self.Icon.setGeometry(150,100,200,150)
         self.nameBox=QLabel("Driver Name",self)
-        self.nameBox.setGeometry(300,10,100,100)
-        self.nameBox.setStyleSheet("
-        ")
+        self.nameBox.setGeometry(300,112,200,28)
+        self.HomeLocation=QLabel("Location/Region",self)
+        self.HomeLocation.setGeometry(300,147,200,28)
+        self.Loyalty=QLabel("Loyalty level:   ",self)
+        self.Loyalty.setGeometry(300,182,200,28)
+        self.nameBox.setObjectName("Texts")
+        self.HomeLocation.setObjectName("Texts")
+        self.Loyalty.setObjectName("Texts")
+
         self.show()
 
         
